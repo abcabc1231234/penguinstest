b9859c1f761027cbead1b342919807769946400b
abcabc1231234 <117754371+abcabc1231234@users.noreply.github.com>
2022-12-08 12:46:00 +0000

    Add files via upload

--- Penguin_Assignment.R
+++ Penguin_Assignment.R
@@ -1,7 +1,10 @@
-#Load the library and functions
+# Set working directory to an appropriate one on your computer
 
-source("libraries.r")
-source("cleaning.r")
+
+#Load the library and functions needed
+source("Libraries.r")
+source("Cleaning.r")
+
 
 # Save the raw penguin data in a folder called "data_raw" and load the data 
 write.csv(penguins_raw, "data_raw/penguins_raw.csv")
@@ -11,6 +14,7 @@ penguins_raw <- read.csv("data_raw/penguins_raw.csv")
 # Clean the raw penguin data and save it in a folder called "data_clean"
 penguins_now_clean <- cleaning(penguins_raw)
 write.csv(penguins_now_clean, "data_clean/penguins_clean.csv")
+
 
 # Subset flipper lengths and body mass data, and remove missing data
 remove_empty_flipperlength_bodymass <- function(data_clean){
@@ -23,12 +27,14 @@ penguins_flippers_bodymass <- remove_empty_flipperlength_bodymass(penguins_now_c
 
 head(penguins_flippers_bodymass)
 
+
 # Create a linear regression model between body mass and flipper length
 linear_model <- lm(body_mass_g ~ flipper_length_mm, penguins_flippers_bodymass)
 
 summary(linear_model)
 
 anova(linear_model)
+
 
 # Plot a scatterplot of flipper length against body mass, with the linear model overlaid
 flipper_bodymass_scatterplot <- ggplot(data = penguins_flippers_bodymass,
@@ -40,12 +46,14 @@ flipper_bodymass_scatterplot <- ggplot(data = penguins_flippers_bodymass,
   theme_bw() +
   scale_colour_manual(values = c("darkorange","purple","cyan4")) + 
   labs(title = "Penguin Body Mass VS Flipper Length", subtitle = "Body Mass and Flipper Lengths for Adelie, Chinstrap and Gentoo Penguins at Palmer Station LTER",
-       x = "Flipper length (mm)", y = "Body mass (g)", colour = "Penguin Species", shape = "Penguin Species")
+       x = "Flipper length (mm)", y = "Body mass (g)", colour = "Penguin Species", shape = "Penguin Species") +
+  scale_x_continuous(breaks = seq(2500, 6500, by = 500)) +
+  scale_y_continuous(breaks = seq(170, 250, by = 10))
 
 flipper_bodymass_scatterplot
 
 
-# Save the figure
+# Save the scatterplot into a png file, and put it in a folder called "figures"
 agg_png("figures/flipper_bodymasss_scatterplot.png", 
         width = 20, height = 20, units = "cm", res = 600, scaling = 1)
 flipper_bodymass_scatterplot
